bfc1bc000813a2eed1f13c11342464a843b3f32f
Mathieu Bosmans <43988246+MATBMS@users.noreply.github.com>
2025-11-02 19:20:57 +0100

    Adding interactivity with the form closes #5

---
+++ scripts/subscription-form.js
@@ -0,0 +1,41 @@
+// Define variables for form and submit button
+const subscriptionForm = document.querySelector('.notify-me-form');
+const submitBtn = document.querySelector('button[type="submit"]');
+const emailField = document.querySelector('.email-field');
+const userEmailInput = document.querySelector('input[name="email"]');
+const emailError = document.querySelector('#email-error');
+const popUp = document.querySelector('#pop-up');
+
+// Function to validate email format
+function isValidEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
+
+// Add event listener for form submission
+subscriptionForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // Handle form submission logic here
+  console.log('Form submitted');
+  const email = userEmailInput.value;
+  console.log('Email:', email);
+  if (!isValidEmail(email)) {
+    userEmailInput.classList.add('error-field');
+    emailError.classList.remove('hide');
+    if (window.innerWidth <= 768) {
+      emailField.style.marginBottom = '24px';
+    }
+  } else {
+    userEmailInput.classList.remove('error-field');
+    emailError.classList.add('hide');
+    emailField.style.marginBottom = '0';
+    // Show pop-up notification
+    popUp.classList.remove('hide');
+    // Reset form after submission
+    subscriptionForm.reset();
+    // Hide pop-up notification
+    setTimeout(() => {
+      popUp.classList.add('slide-down');
+    }, 3000); // Hide after 3 seconds
+  }
+});
